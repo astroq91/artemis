c5dba276dc0e5a7a49551944dc552c71ee69b4a5
Jonatan Lindell <lindelljonatan57@gmail.com>
2025-11-22 23:00:56 +0100

    events test

--- tests/artemis/test_event_bus.cpp
+++ tests/artemis/test_event_bus.cpp
@@ -1,3 +1,23 @@
 #include "gtest/gtest.h"
+#include <artemis/events/event.hpp>
+#include <artemis/events/event_bus.hpp>
 
-TEST(artemis, event_bus) { EXPECT_EQ(1, 6); }
+using namespace artemis;
+
+struct TestEvent : Event {
+    std::string str;
+    int a;
+};
+
+TEST(artemis, test_publish) {
+    TestEvent event;
+    event.str = "string!";
+    event.a = 64;
+
+    EventBus bus;
+    bus.subscribe<TestEvent>([&](const TestEvent& e) {
+        ASSERT_EQ("string!", e.str);
+        ASSERT_EQ(64, e.a);
+    });
+    bus.publish(event);
+}
